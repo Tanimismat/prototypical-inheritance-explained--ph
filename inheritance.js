81f367a65b6839196a4a6f8612f0e12165fd31db
Tanim Ismat <tanim.ismat@gmail.com>
2022-07-06 20:33:29 +0600

    super ()

--- inheritance.js
+++ inheritance.js
@@ -1,4 +1,4 @@
-class Support {
+class TeamMember{
     name;
     designation = "Support web dev";
     address = "BD";
@@ -6,23 +6,40 @@ class Support {
         this.name = name;
         this.address = address;
     }
+}
+
+class Support extends TeamMember {
     startSession() {
         console.log(this.name, "start session");
     }
 }
-class StudentCare {
-    name;
-    designation = "Support web dev";
-    address = "BD";
-    constructor(name, address) {
-        this.name = name;
-        this.address = address;
+class StudentCare extends TeamMember{
+    groupSupportTime;
+    constructor(name, address, time) {
+        super(name, address)
+        this.groupSupportTime = time;
     }
     buildARoutine() {
         console.log(this.name, "Build a routine");
     }
 }
+class NeptuneDev {
+    name;
+    designation = "Support web dev";
+    address = "BD";
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+    releaseApp(version) {
+        console.log(this.name, "Release App", version);
+    }
+}
 
 const amir = new Support("Amir Khan", "BD");
-const salman = new StudentCare("Salman khan", "Dubai")
-console.log(salman)
+const salman = new StudentCare("Salman khan", "Dubai" , 11)
+const srk = new NeptuneDev("SRK", "Dubai", 3)
+srk.releaseApp('1.2.4')
+// console.log(amir)
+// console.log(salman)
+console.log(srk)
